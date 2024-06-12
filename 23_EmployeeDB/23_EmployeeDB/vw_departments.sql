acfbd19bcd214cd66e37e0522388cd044bcb8d57
Djulizah <djul.b74123@gmail.com>
2024-06-12 11:43:27 +0800

    add: views

---
+++ 23_EmployeeDB/23_EmployeeDB/vw_departments.sql
@@ -0,0 +1,9 @@
+-- =============================================
+-- Author:		Raden Fakhri R
+-- Create date: 03-06-2024
+-- Description:	<Description,,>
+-- =============================================
+
+CREATE VIEW view_all_departments AS
+SELECT *
+FROM tbl_departments d
