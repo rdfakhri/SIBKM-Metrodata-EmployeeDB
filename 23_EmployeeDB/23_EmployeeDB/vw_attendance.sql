ba21038458f195e97c9902f8c673f96a718364e8
Djulizah <djul.b74123@gmail.com>
2024-06-14 03:44:55 +0800

    add: views

--- 23_EmployeeDB/23_EmployeeDB/vw_attendance.sql
+++ 23_EmployeeDB/23_EmployeeDB/vw_attendance.sql
@@ -8,5 +8,5 @@ CREATE VIEW attendance_details
 AS
 	SELECT a.id, CONCAT(e.first_name, ' ', e.last_name) AS 'Full Name', e.email, a.time
 	FROM tbl_attendance a
-	INNER JOIN tbl_employees e ON a.employee = e.id;  -- Added join condition
-GO	
+	INNER JOIN tbl_employees e ON a.employee = e.id;
+GO
