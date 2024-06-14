81ed46629bd593ce63231bfc82851b02475b9607
Djulizah <djul.b74123@gmail.com>
2024-06-15 01:10:59 +0800

    add: profile stored procedures

---
+++ 23_EmployeeDB/23_EmployeeDB/sp_edit_password.sql
@@ -0,0 +1,57 @@
+-- =============================================
+-- Author:		Djulizah B
+-- Create date: 15-06-2024
+-- Description:	<Description,,>
+-- =============================================
+
+CREATE PROCEDURE editPassword (
+  @email varchar(25),
+  @password varchar,
+  @newPassword varchar(255),
+  @confirmPassword varchar(255)
+)
+AS
+BEGIN
+  DECLARE @errorMessage nvarchar(500);
+  DECLARE @userId int;
+
+  BEGIN TRY
+    -- Check email exists in tbl_accounts
+    SELECT @userId = id FROM tbl_accounts WHERE username = @email;
+
+    IF @userId IS NULL
+    BEGIN
+      SET @errorMessage = 'Username not found';
+      RAISERROR ('Error: %s', 16, 1, @errorMessage);
+    END
+    ELSE
+    BEGIN
+      -- Validate new password (call IsValidPassword function)
+      DECLARE @isValidPassword bit;
+      SET @isValidPassword = dbo.isValidPassword(@newPassword);
+
+      IF @isValidPassword = 0
+      BEGIN
+        SET @errorMessage = 'Invalid new password requirements';
+        RAISERROR ('Error: %s', 16, 1, @errorMessage);
+      END
+      ELSE IF @newPassword <> @confirmPassword
+      BEGIN
+        SET @errorMessage = 'New password and confirm password do not match';
+        RAISERROR ('Error: %s', 16, 1, @errorMessage);
+      END
+      ELSE
+      BEGIN
+        -- Update password only if new password and confirm password match
+        UPDATE tbl_accounts
+        SET password = @newPassword
+        WHERE id = @userId;
+      END
+    END
+  END TRY
+
+  BEGIN CATCH
+    SET @errorMessage = ERROR_MESSAGE();
+    RAISERROR ('Error resetting password: %s', 16, 1, @errorMessage);
+  END CATCH;
+END;
---
+++ 23_EmployeeDB/23_EmployeeDB/sp_edit_profile.sql
@@ -0,0 +1,73 @@
+-- =============================================
+-- Author:		Djulizah B
+-- Create date:	14-06-2024
+-- Description:	<Description,,>
+-- =============================================
+
+CREATE PROCEDURE editEmployee (
+	@id int,
+	@firstName varchar(25),
+	@lastName varchar(25),
+	@gender varchar(10),
+	@email varchar(25),
+	@phone varchar(20),
+	@hireDate date,
+	@salary int,
+	@managerId int,
+	@jobId varchar(10), 
+	@departmentId int
+)
+AS
+BEGIN
+  DECLARE @errorMessage nvarchar(500);
+
+  BEGIN TRY
+    -- Validate data using functions (call them before insert)
+    IF @gender = '' OR dbo.isValidGender(@gender) = 0
+    BEGIN
+      SET @errorMessage = 'Invalid gender provided.';
+      RAISERROR ('Error adding employee: %s', 16, 1, @errorMessage);
+      RETURN;
+    END;
+
+    IF dbo.isValidEmail(@email) = 0
+    BEGIN
+      SET @errorMessage = 'Invalid email address.';
+      RAISERROR ('Error adding employee: %s', 16, 1, @errorMessage);
+      RETURN;
+    END;
+
+    IF dbo.IsNumericPhoneNumber(@phone) = 0
+    BEGIN
+      SET @errorMessage = 'Invalid phone number format.';
+      RAISERROR ('Error adding employee: %s', 16, 1, @errorMessage);
+      RETURN;
+    END;
+
+    IF dbo.isValidSalary(@salary) = 0  -- Implement logic for min/max salary in function
+    BEGIN
+      SET @errorMessage = 'Salary is outside the allowed range.';
+      RAISERROR ('Error adding employee: %s', 16, 1, @errorMessage);
+      RETURN;
+    END;
+
+    UPDATE tbl_employees
+	SET first_name = @firstName,
+		last_name = @lastName,
+		gender = @gender,
+		email = @email,
+		phone = @phone,
+		hire_date = @hireDate,
+		salary = @salary,
+		manager = @managerId,
+		job = @jobId,
+		department = @departmentId
+    WHERE id = @id;
+  END TRY
+
+  BEGIN CATCH
+    SET @errorMessage = ERROR_MESSAGE();
+    RAISERROR ('Error adding employee: %s', 16, 1, @errorMessage);
+  END CATCH;
+
+END;
