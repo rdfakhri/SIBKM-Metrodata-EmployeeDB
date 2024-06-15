bf6ae0af22dee13e44913b5baf1c4502bd24a628
Djulizah <djul.b74123@gmail.com>
2024-06-16 04:59:39 +0800

    add: symbol and number checker

--- 23_EmployeeDB/23_EmployeeDB/func_password_policy.sql
+++ 23_EmployeeDB/23_EmployeeDB/func_password_policy.sql
@@ -1,14 +1,14 @@
 -- =============================================
--- Author:	Djulizah B
+-- Author: Djulizah B
 -- Create date: 12-06-2024
--- Description:	<Description,,>
+-- Description:	Function to check password requirements
 -- =============================================
 
-CREATE FUNCTION isValidPassword(@password varchar(50))
+CREATE FUNCTION isValidPassword(@password varchar(255))
 RETURNS bit AS
 BEGIN
   DECLARE @isValid bit;
-  SET @isValid = 0; -- Assume invalid initially
+  SET @isValid = 0;
 
   -- Check minimum length
   IF LEN(@password) < 8
@@ -19,24 +19,24 @@ BEGIN
   -- Check for at least one uppercase letter
   DECLARE @hasUppercase bit;
   SET @hasUppercase = 0;
-  DECLARE @char int = 1; -- Loop counter variable
+  DECLARE @char int = 1;
 
-  WHILE @char <= LEN(@password)  -- Use WHILE loop
+  WHILE @char <= LEN(@password) 
   BEGIN
     IF ASCII(SUBSTRING(@password, @char, 1)) >= 65 AND ASCII(SUBSTRING(@password, @char, 1)) <= 90
     BEGIN
       SET @hasUppercase = 1;
       BREAK;
     END;
-    SET @char = @char + 1; -- Increment counter
+    SET @char = @char + 1;
   END;
 
   IF @hasUppercase = 0
   BEGIN
-    RETURN @isValid; -- No uppercase letter
+    RETURN @isValid;
   END;
 
-  -- Check for at least one lowercase letter (similar logic)
+  -- Check for at least one lowercase letter
   DECLARE @hasLowercase bit;
   SET @hasLowercase = 0;
   SET @char = 1; -- Reset counter
@@ -53,15 +53,55 @@ BEGIN
 
   IF @hasLowercase = 0
   BEGIN
-    RETURN @isValid; -- No lowercase letter
+    RETURN @isValid;
   END;
 
-  -- Similar logic for digit and symbol checks using WHILE loops
+    -- Check for at least one number (digit)
+  DECLARE @hasNumber bit;
+  SET @hasNumber = 0;
+  SET @char = 1;
+
+  WHILE @char <= LEN(@password)
+  BEGIN
+    IF ASCII(SUBSTRING(@password, @char, 1)) >= 48 AND ASCII(SUBSTRING(@password, @char, 1)) <= 57
+    BEGIN
+      SET @hasNumber = 1;
+      BREAK;
+    END;
+    SET @char = @char + 1;
+  END;
+
+  IF @hasNumber = 0
+    BEGIN
+      RETURN @isValid;
+    END;
+
+	-- Check for at least one symbol
+  DECLARE @hasSymbol bit;
+  SET @hasSymbol = 0;
+  SET @char = 1;
+
+  WHILE @char <= LEN(@password)
+  BEGIN
+    -- Check for any character that is not a letter, number, or underscore
+    IF (ASCII(SUBSTRING(@password, @char, 1)) < 48 OR ASCII(SUBSTRING(@password, @char, 1)) > 57) AND 
+       (ASCII(SUBSTRING(@password, @char, 1)) < 65 OR ASCII(SUBSTRING(@password, @char, 1)) > 90) AND 
+       (ASCII(SUBSTRING(@password, @char, 1)) < 97 OR ASCII(SUBSTRING(@password, @char, 1)) > 122) AND
+       ASCII(SUBSTRING(@password, @char, 1)) <> 95  -- Exclude underscore
+    BEGIN
+      SET @hasSymbol = 1;
+      BREAK;
+    END;
+    SET @char = @char + 1;
+  END;
+
+  IF @hasSymbol = 0
+    BEGIN
+      RETURN @isValid;
+    END;
 
   -- All criteria met
   SET @isValid = 1;
-
-  -- Added the missing RETURN statement
   RETURN @isValid;
 END;
 GO
