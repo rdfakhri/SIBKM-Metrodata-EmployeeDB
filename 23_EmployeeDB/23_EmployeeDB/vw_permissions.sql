22d40a3334aa7660b4eae2131137580b808f982a
Djulizah <djul.b74123@gmail.com>
2024-06-16 04:52:57 +0800

    deleted: renamed with _details

--- 23_EmployeeDB/23_EmployeeDB/vw_permissions.sql
+++
@@ -1,9 +0,0 @@
--- =============================================
--- Author:		Raden Fakhri R
--- Create date: 03-06-2024
--- Description:	<Description,,>
--- =============================================
-
-CREATE VIEW view_all_permissions AS
-SELECT id, name
-FROM tbl_permissions;
