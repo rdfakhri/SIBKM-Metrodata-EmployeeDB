da095ba4dbdec162bf8eed1e0eacb457d09a8811
Djulizah <djul.b74123@gmail.com>
2024-06-17 15:57:58 +0800

    fix: tbl_attendances renamed

--- 23_EmployeeDB/23_EmployeeDB/vw_attendance_details.sql
+++ 23_EmployeeDB/23_EmployeeDB/vw_attendance_details.sql
@@ -7,6 +7,6 @@
 CREATE VIEW attendance_details
 AS
 	SELECT a.id, CONCAT(e.first_name, ' ', e.last_name) AS 'Full Name', e.email, a.time
-	FROM tbl_attendance a
+	FROM tbl_attendances a
 	INNER JOIN tbl_employees e ON a.employee = e.id;
 GO
